b27863295fd3b0910b8a1654bddecc9aa646c681
Jonas <jonas.gerstl@outlook.com>
2023-11-17 10:37:47 +0100

    Models Test

--- WGIdeeApp/models.py
+++ WGIdeeApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    wert = models.FloatField(default=0.00)
+
+class Student(Person):
+    matrikelnr = models.IntegerField(max_length=7)
+
+    
