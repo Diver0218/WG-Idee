8d69464e8c1b96c0116836f7bd20f1e08337cfa5
Diver0218 <jamin.bareis@gmail.com>
2024-02-27 15:01:26 +0100

    removed calcuations comment

--- WGIdeeApp/calculations.py
+++ WGIdeeApp/calculations.py
@@ -14,65 +14,6 @@ def calculate_debts_all(Person_list, Ausgaben_list):
 
 
 #################
-#region Compensation original (not working)
-#################
-#
-#def compensation(Ausgaben_list, Person_list) -> list:
-#
-#    calculate_debts_all(Person_list, Ausgaben_list)
-#
-#    comp_list = []
-#    #list_object = {'sender': Person, 'receiver': Person, 'Amount': int}
-#
-#    Person_list_not_excluded = Person_list
-#
-#    for p in Person_list_not_excluded:
-#        print(p)
-#        if p.debts == 0:
-#            Person_list_not_excluded = Person_list_not_excluded.exclude(pk=p.id)
-#
-#    return compensation_recursive(Ausgaben_list, Person_list_not_excluded, comp_list)
-#            
-#
-#def compensation_recursive(Ausgaben_list, Person_list_not_excluded, comp_list: list) -> list:
-#
-#    Person_list_not_excluded.order_by("-debts")
-#
-#    #Rekursionsbasis
-#    if Person_list_not_excluded.count() == 1:
-#        print(comp_list)
-#        return comp_list
-#
-#    if Person_list_not_excluded.last().debts < abs(Person_list_not_excluded.first().debts):
-#        to_pay = Person_list_not_excluded.last().debts
-#        Person_list_not_excluded.last().debts = 0
-#        Person_list_not_excluded = Person_list_not_excluded.exclude(pk=Person_list_not_excluded.last().id)
-#        Person_list_not_excluded.first().debts += to_pay
-#        #print(to_pay)
-#        #print(Person_list_not_excluded.last())
-#        #print(Person_list_not_excluded.first())
-#    else:
-#        to_pay = abs(Person_list_not_excluded.first().debts)
-#        Person_list_not_excluded.first().debts = 0
-#        Person_list_not_excluded = Person_list_not_excluded.exclude(pk=Person_list_not_excluded.first().id)
-#        Person_list_not_excluded.last().debts -= to_pay
-#        #print(to_pay)
-#        #print(Person_list_not_excluded.last())
-#        #print(Person_list_not_excluded.first())
-#
-#    comp_list_object ={
-#            'sender': Person_list_not_excluded.last(),
-#            'receiver': Person_list_not_excluded.first(),
-#            'amount': to_pay
-#        }
-#    print(comp_list_object)
-#    comp_list.append(comp_list_object)
-#    compensation_recursive(Ausgaben_list, Person_list_not_excluded, comp_list)
-#    return comp_list
-#endregion
-
-
-#################
 #region Compensation testing
 #################
 
