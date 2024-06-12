e7ec53c4744b95640ff3a74413be34bccb89fe93
Diver0218 <jamin.bareis@gmail.com>
2024-06-12 15:02:39 +0200

    chenged url for demo

--- WGIdee/urls.py
+++ WGIdee/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('WGIdee/', include('WGIdeeApp.urls')),
-    path('WGIdee/', include('django.contrib.auth.urls')),
+    path('', include('WGIdeeApp.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
     
